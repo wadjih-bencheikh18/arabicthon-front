8a363f043e211261ac12584e278d4997a8440b11
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-24 17:08:32 +0100

    feat: add techkil

--- src/components/Tachkil.jsx
+++ src/components/Tachkil.jsx
@@ -1,27 +1,58 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Tachkil() {
   const [input, setInput] = useState({
-    letters: ["a", "b", "c", "d", "e"],
-    value: "",
+    letters: [
+      "\u064E",
+      "\u064F",
+      "\u0650",
+      "\u064B",
+      "\u064C",
+      "\u064D",
+      "\u0651",
+      "\u0652",
+    ],
+    value: "مساء الخير",
+    start: 0,
   });
-
+  const inputRef = useRef(null);
   function updateValue(letter) {
-    console.log("ran");
-    setInput(({ value, letters }) => ({
-      letters,
-      value: value + letter,
+    setInput(({ value, start, ...others }) => ({
+      ...others,
+      start: start + 1,
+      value: value.slice(0, start) + letter + value.slice(start),
     }));
   }
 
-  const createButtons = input.letters.map((letter) => (
-    <button onClick={() => updateValue(letter)}>{letter}</button>
+  const createButtons = input.letters.map((letter, i) => (
+    <button
+      style={{ fontSize: "30px", width: "50px" }}
+      key={i}
+      onClick={() => updateValue(letter)}
+    >
+      {letter}
+    </button>
   ));
 
   return (
     <div>
-      {createButtons}
-      <input style={{ border: "2px solid red" }} value={input.value} />
+      <input
+        ref={inputRef}
+        onClick={({ target }) => {
+          const start = target.selectionStart;
+          if (start) setInput((input) => ({ ...input, start }));
+        }}
+        value={input.value}
+        onChange={({ target }) => {
+          alert(target);
+          setInput((input) => ({
+            ...input,
+            value: target.value,
+          }));
+        }}
+      />
+
+      <div>{createButtons}</div>
     </div>
   );
 }
