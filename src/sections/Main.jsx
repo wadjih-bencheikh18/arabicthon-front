6751565b396bd5834feeb152b012f96b7978a8d8
moonayyur <jm_kias@esi.dz>
2022-05-26 20:33:22 +0100

    hero bg

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,7 +1,7 @@
 export function Main() {
   return (
     <div className="flex h-screen items-center justify-center overflow-hidden">
-      {/* <img src="./whiterectangle.png" alt="" className="" /> */}
+      <img src="./bg.png" alt="" className="" />
     </div>
   );
 }
