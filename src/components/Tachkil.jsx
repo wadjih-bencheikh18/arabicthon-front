a275ccb0fbaf85a421ba9ef3dd588bfb2770279c
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-26 13:11:24 +0100

    add features

--- src/components/Tachkil.jsx
+++ src/components/Tachkil.jsx
@@ -21,9 +21,9 @@ export default function Tachkil() {
     if (select) {
       let start = 0;
       if (select === 0 || input.value[select - 1] === " ") start = select + 1;
-      else {
-        start = select;
-      }
+      else if (select > input.value.length) {
+        start = input.value.length;
+      } else start = select;
       setInput((input) => ({ ...input, start }));
     }
   }
@@ -87,39 +87,74 @@ export default function Tachkil() {
   function stringCol() {
     const str = [];
     for (let i = 0; i < input.value.length; i++) {
-      if (
+      if (i === input.start - 1) {
+        str.push(<span className=" text-green-600">{input.value[i]}</span>);
+      } else if (
         !cLetters.includes(input.value[i]) &&
         ((i + 1 < input.value.length &&
           !cLetters.includes(input.value[i + 1])) ||
           input.value[i + 1] === undefined)
       ) {
         str.push(<span className=" text-red-600">{input.value[i]}</span>);
-      } else
-        str.push(<span className=" text-green-500">{input.value[i]}</span>);
+      } else str.push(<span>{input.value[i]}</span>);
     }
     return str;
   }
   const createButtons = cLetters.map((letter, i) => (
     <button
       key={i}
-      className="border-black border-2 mt-10 mx-2"
+      className="border-black relative border-2 mt-10 mx-2"
       style={{ fontSize: "30px", width: "50px" }}
       onClick={() => updateValue(letter)}
     >
-      {letter}
+      <div>{letter}</div>
+      <div className="bg-black px-1 absolute text-white -bottom-2 -left-1 text-sm">
+        {i + 1}
+      </div>
     </button>
   ));
 
   return (
-    <div className="mt-32">
+    <div
+      className="mt-32 outline-none"
+      tabindex="0"
+      onKeyDown={({ key }) => {
+        if (key && ["1", "2", "3", "4", "5", "6", "7", "8"].includes(key))
+          updateValue(cLetters[Number(key) - 1]);
+
+        if (key === "ArrowRight") {
+          let i = input.start + (input.start > 1 ? -1 : 0);
+          for (
+            ;
+            i > 1 &&
+            (cLetters.includes(input.value[i - 1]) ||
+              input.value[i - 1] === " ");
+            i--
+          );
+          setStart(i);
+        }
+        if (key === "ArrowLeft") {
+          let i =
+            input.start <= input.value.length ? input.start + 1 : input.start;
+          for (
+            ;
+            i < input.value.length &&
+            (cLetters.includes(input.value[i - 1]) ||
+              input.value[i - 1] === " ");
+            i++
+          );
+          setStart(i);
+        }
+      }}
+    >
       <div className="relative w-52 mx-auto">
-        <div className="border-black -z-0 bg-white left-0 absolute top-0 border-2 text-right w-52 mx-auto">
+        <div className="border-black -z-0  bg-white left-0 absolute top-0 border-2 text-right w-52 mx-auto">
           {stringCol()}
         </div>
         <input
           spellCheck="false"
           style={{ direction: "rtl" }}
-          className="border-black border-2 z-20 left-0 caret-black absolute top-0 bg-transparent text-[rgba(0,0,0,0)] text-right w-52 "
+          className="border-black border-2 z-20 left-0  absolute top-0 caret-transparent bg-transparent text-[rgba(0,0,0,0)] text-right w-52 "
           ref={inputRef}
           onClick={({ target }) => {
             setStart(target.selectionStart);
@@ -127,14 +162,16 @@ export default function Tachkil() {
           value={input.value}
           onChange={({ target }) => {
             if (
-              (target.value.length > input.value.length &&
+              !["1", "2", "3", "4", "5", "6", "7", "8"].includes(
+                target.value[input.value.length]
+              ) &&
+              ((target.value.length > input.value.length &&
                 cLetters.includes(target.value[input.start])) ||
-              (target.value.length < input.value.length &&
-                cLetters.includes(input.value[input.start - 1]))
+                (target.value.length < input.value.length &&
+                  cLetters.includes(input.value[input.start - 1])))
             ) {
               setInput((input) => ({
                 ...input,
-                start: target.selectionStart,
                 value: target.value,
               }));
             }
