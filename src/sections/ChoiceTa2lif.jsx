309cb99935bcf0fae64b44d8df29128a8ccf7f10
moonayyur <jm_kias@esi.dz>
2022-05-27 15:51:40 +0100

    change struct

--- src/sections/ChoiceTa2lif.jsx
+++ src/sections/ChoiceTa2lif.jsx
@@ -1,40 +1,43 @@
+import { useState } from "react";
+
 export function ChoiceTa2lif() {
+  const [index, setIndex] = useState(0);
   const functionalities = [
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
-    "k",
-    "text1",
+    {
+      short: "text1",
+      long: "descrip1",
+    },
+    {
+      short: "text2",
+      long: "descrip2",
+    },
+    {
+      short: "text3",
+      long: "descrip3",
+    },
+    {
+      short: "text4",
+      long: "descrip4",
+    },
   ];
   return (
     <div className="flex h-screen pt-10 justify-end items-center">
       <div className="text-right border-r-4 rounded-sm border-[#A58453] pr-8">
-        <div className="text-3xl text-[#A58453]">تأليف شعر</div>
-        <div className="text-lg">تأليف شعر</div>
+        <div className="text-3xl text-[#A58453]">
+          {functionalities[index].short}
+        </div>
+        <div className="text-lg">{functionalities[index].long}</div>
       </div>
+
       {/* container */}
       <div className="flex flex-wrap flex-row-reverse h-[31.5rem] w-[54rem] mx-20 overflow-y-scroll items-center px-5 pt-4">
         {functionalities.map((functionality, key) => (
           <div
+            onClick={() => setIndex((index) => functionality.index)}
             key={key}
             className="bg-[#FBFAF8] rounded-[40px] w-60 h-56 flex justify-center items-center mr-6 mb-6"
           >
-            {functionality}
+            {functionality.short}
           </div>
         ))}
       </div>
