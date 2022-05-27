7115316d4ce3b2644d79c67c1ec656f3995efc06
moonayyur <jm_kias@esi.dz>
2022-05-27 11:42:19 +0100

    add choice ta2lif

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from "./sections/Navbar";
 import AnalysePoem from "./components/AnalysePoem";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ChoiceMain } from "./sections/ChoiceMain";
+import { ChoiceTa2lif } from "./sections/ChoiceTa2lif";
 function App() {
   return (
     <Router>
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route path="/create-analyse">
             <ChoiceMain />
+          </Route>
+          <Route path="/create">
+            <ChoiceTa2lif />
           </Route>
           <Route path="/analyse">
             <div className="h-screen pt-20">
---
+++ src/sections/ChoiceTa2lif.jsx
@@ -0,0 +1,14 @@
+export function ChoiceTa2lif() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      {/* container */}
+      <div className="flex justify-center items-center">
+        <div className="bg-[#FBFAF8] rounded-[40px] w-80 h-72 flex justify-center items-center">
+          text
+        </div>
+      </div>
+      {/* text */}
+      <div className=""></div>
+    </div>
+  );
+}
