5395856cd389ac8dbd6f710fabedf65b26956076
moonayyur <jm_kias@esi.dz>
2022-05-27 09:24:56 +0100

    bg test

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,12 +1,15 @@
 export function Main() {
   return (
     <div className="flex h-screen items-center justify-center overflow-hidden">
+      <div className="absolute -z-10">
+        <img src="./whiterectangle.png" alt="" className="relative" />
+      </div>
       <div className="hidden lg:block">
         <img src="./book.png" alt="" className="w-[100em] mr-[50em] mt-52" />
       </div>
       {/* text */}
       <div className="text-right mr-52 mb-32">
-        <div className="text-3xl mb-10">عنوان</div>
+        <div className="text-3xl mb-10 text-[#A58453]">عنوان</div>
         <div className="">
           تأليف شعر تأليف شعر تأليف شعر تأليف شعر تأليف شعر تأليف شعر تأليف شعر
           تأليف شعر تأليف شعر تأليف شعر تأليف شعر تأليف شعر تأليف شعر تأليف شعر
