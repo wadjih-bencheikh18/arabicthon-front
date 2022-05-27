4debdfe5a1dd931a5a67033a4e6531814513a358
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-27 20:17:39 +0100

    update

--- src/components/Functional/InputResult.jsx
+++ src/components/Functional/InputResult.jsx
@@ -12,8 +12,11 @@ export default function InputResult({
   return (
     <div className="flex flex-row-reverse items-end mt-5">
       <textarea
-        className={`${className} resize-none border-black py-3 border-2 text-center rounded-md input`}
-        cols={Math.max(...input.split("\n").map((s) => s.length), cols)}
+        className={`${className} resize-none border-black py-3 px-3 border-2 text-center rounded-md input`}
+        cols={Math.min(
+          Math.max(...input.split("\n").map((s) => s.length), cols),
+          100
+        )}
         rows={Math.max(input.split("\n").length, rows)}
         value={input}
         placeHolder={placeHolder}
@@ -21,8 +24,8 @@ export default function InputResult({
         style={{ direction: "rtl" }}
       ></textarea>
       <button
-        onClick={({ key }) => {
-          if (key === "Enter") setValue(input);
+        onClick={() => {
+          setValue(input);
         }}
         className="btn flex rounded-md items-center justify-center w-12 h-6 text-white text-lg bg-[#A58453] -mb-3 -mr-9"
       >
--- src/components/Functional/OutputResult.jsx
+++ src/components/Functional/OutputResult.jsx
@@ -7,8 +7,11 @@ export default function OutputResult({
   return (
     <textarea
       style={{ direction: "rtl" }}
-      className={`${className} resize-none bg-[#FBFAF8] rounded-3xl p-4 mt-5`}
-      cols={Math.max(...value.split("\n").map((s) => s.length), cols)}
+      className={`${className} resize-none  py-3 px-3  bg-[#FBFAF8] text-center rounded-md p-4 mt-5`}
+      cols={Math.min(
+        Math.max(...value.split("\n").map((s) => s.length), cols),
+        100
+      )}
       rows={Math.max(value.split("\n").length, rows)}
       disabled
     >
