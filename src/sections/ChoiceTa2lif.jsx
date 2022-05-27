6481b94dcde862921ccf71ce8908e857e57298aa
moonayyur <jm_kias@esi.dz>
2022-05-27 13:26:46 +0100

    change ta2lif choice page logic

--- src/sections/ChoiceTa2lif.jsx
+++ src/sections/ChoiceTa2lif.jsx
@@ -1,29 +1,41 @@
 export function ChoiceTa2lif() {
   const functionalities = [
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
-    { link: "k", text: "text1" },
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
+    "k",
+    "text1",
   ];
   return (
     <div className="flex h-screen pt-10 justify-end items-center">
-      <div className="text-3xl">تأليف شعر</div>
+      <div className="text-right border-r-4 rounded-sm border-[#A58453] pr-8">
+        <div className="text-3xl text-[#A58453]">تأليف شعر</div>
+        <div className="text-lg">تأليف شعر</div>
+      </div>
       {/* container */}
       <div className="flex flex-wrap flex-row-reverse h-[31.5rem] w-[54rem] mx-20 overflow-y-scroll items-center px-5 pt-4">
         {functionalities.map((functionality, key) => (
-          <a
-            href={functionality.link}
+          <div
             key={key}
             className="bg-[#FBFAF8] rounded-[40px] w-60 h-56 flex justify-center items-center mr-6 mb-6"
           >
-            {functionality.text}
-          </a>
+            {functionality}
+          </div>
         ))}
       </div>
     </div>
