898adb35bb9cea3853b115e6728f49c0177b96da
moonayyur <jm_kias@esi.dz>
2022-05-27 12:27:52 +0100

    init ta2lif choice page

--- src/sections/ChoiceTa2lif.jsx
+++ src/sections/ChoiceTa2lif.jsx
@@ -1,14 +1,33 @@
 export function ChoiceTa2lif() {
+  const functionalities = [
+    "text1",
+    "text2",
+    "jlk",
+    "iukhj",
+    "iuhk",
+    "text2",
+    "jlk",
+    "iukhj",
+    "iuhk",
+    "text2",
+    "jlk",
+    "iukhj",
+    "iuhk",
+  ];
   return (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex h-screen mx-5 pt-10 justify-center items-center">
+      <div className="w-full">text</div>
       {/* container */}
-      <div className="flex justify-center items-center">
-        <div className="bg-[#FBFAF8] rounded-[40px] w-80 h-72 flex justify-center items-center">
-          text
-        </div>
+      <div className="flex flex-wrap h-[75%] mx-36 overflow-y-scroll items-center px-5 border-black border-2 pt-4">
+        {functionalities.map((functionality, key) => (
+          <div
+            key={key}
+            className="bg-[#FBFAF8] rounded-[40px] w-60 h-56 flex justify-center items-center mr-6 mb-6"
+          >
+            {functionality}
+          </div>
+        ))}
       </div>
-      {/* text */}
-      <div className=""></div>
     </div>
   );
 }
