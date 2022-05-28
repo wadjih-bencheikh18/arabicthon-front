9ca73c9aef66cd65de7b299eafc3b708dd569ed1
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-28 03:55:35 +0100

    update

--- src/components/Functional/Table.jsx
+++ src/components/Functional/Table.jsx
@@ -1,24 +1,34 @@
-export default TableArray ({aroud,harakat,tafil}) {
-
-    return(
-        <table>
-        
-            <tr>
-                <th>aroud</th>
-                <td>Maria Anders</td>
-                <td>Germany</td>
-            </tr>
-            <tr>
-                <th>harakat</th>
-                <td>Francisco Chang</td>
-                <td>Mexico</td>
-            </tr>
-            <tr>
-                <th>tafil</th>
-                <td>Francisco Chang</td>
-                <td>Mexico</td>
-            </tr>
-        </table>
-    )
-
+export default function TableArray({ aroud, harakat, tafil, meter, ratio }) {
+  const className = "border border-black p-2";
+  const Aroud = aroud.map((a) => <td className={className}>{a}</td>);
+  const Harakat = harakat.map((a) => <td className={className} >{a}</td>);
+  const Tafil = tafil.map((a) => <td className={className}>{a}</td>);
+  return (
+    <table className="border-collapse mt-5">
+      <tr>
+        <th className={className}>aroud</th>
+        {Aroud}
+      </tr>
+      <tr>
+        <th className={className}>harakat</th>
+        {Harakat}
+      </tr>
+      <tr>
+        <th className={className}>tafil</th>
+        {Tafil}
+      </tr>
+      <tr>
+        <th className={className}>meter</th>
+        <td className={className} colSpan={aroud.length}>
+          {meter}
+        </td>
+      </tr>
+      <tr>
+        <th className={className}>ratio</th>
+        <td className={className} colSpan={aroud.length}>
+          {ratio}
+        </td>
+      </tr>
+    </table>
+  );
 }
