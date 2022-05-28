07ece05f922ed279361c44769228378b4db8ab83
moonayyur <jm_kias@esi.dz>
2022-05-28 06:27:27 +0100

    fix prblms

--- src/components/Functional/Ta9ti3.jsx
+++ src/components/Functional/Ta9ti3.jsx
@@ -2,7 +2,7 @@ import Analyse from "./Analyse";
 
 export default function Ta9ti3() {
   return (
-    <div className="pb-32 bg-[#E4D3C1]">
+    <div className="pb-32 bg-[#E4D3C1] min-h-screen">
       <div className="text-3xl text-[#A58453] pt-32 mx-80">تقطيع الشعر</div>
       <Analyse activate={[0, 1, 2]} />
     </div>
--- src/components/Functional/Taf3ilat.jsx
+++ src/components/Functional/Taf3ilat.jsx
@@ -2,7 +2,7 @@ import Analyse from "./Analyse";
 
 export default function Taf3ilat() {
   return (
-    <div className="pb-32 bg-[#E4D3C1]">
+    <div className="pb-32 bg-[#E4D3C1] min-h-screen">
       <div className="text-3xl text-[#A58453] pt-32 mx-80">
         التعرف على وزن و تفعيلات شعر
       </div>
