a870c2051d906033fa4850edca3a9cb61dc6cda4
moonayyur <jm_kias@esi.dz>
2022-05-29 21:08:54 +0100

    fix chi3r component

--- src/components/Functional/Chiir.jsx
+++ src/components/Functional/Chiir.jsx
@@ -1,20 +1,21 @@
-import {  useMemo } from "react";
+import { useMemo } from "react";
 
-export default function Chiir({result}){
-    const [sdr, ajz] = useMemo(() => {
-      const sdr = [];
-      const ajz = [];
-      console.log(result);
-      result.split(/[_]|\n/).forEach((r, i) => {
-        if (i % 2 === 0) {
-          sdr.push(r);
-        } else ajz.push(r);
-      });
-      return [sdr, ajz];
-    }, [result]);
-   
-    return (
-      <table className="w-[700px] text-xl">
+export default function Chiir({ result }) {
+  const [sdr, ajz] = useMemo(() => {
+    const sdr = [];
+    const ajz = [];
+    console.log(result);
+    result.split(/[_]|\n/).forEach((r, i) => {
+      if (i % 2 === 0) {
+        sdr.push(r);
+      } else ajz.push(r);
+    });
+    return [sdr, ajz];
+  }, [result]);
+
+  return (
+    <div className="py-4 bg-[#FBFAF8] rounded-md border-2 border-[#A58453]">
+      <table className="w-[700px] text-xl text-center min-w-20">
         <tbody>
           {sdr.map((sdr, i) => (
             <tr key={i}>
@@ -24,5 +25,6 @@ export default function Chiir({result}){
           ))}
         </tbody>
       </table>
-    );
+    </div>
+  );
 }
